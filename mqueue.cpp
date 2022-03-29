d1370dc1d040c2ea1c076da80ac605a172201e32
Bajad3rka <102176673+Bajad3rka@users.noreply.github.com>
2022-03-29 18:43:13 +0200

    Add files via upload

---
+++ mqueue.cpp
@@ -0,0 +1,18 @@
+/*
+class implementing queue in C++
+the same functionality as STL (Standard Template Library) class queue in C++
+with usage example
+version 1.0
+Consists
+a. Class definition
+Author: Bajadera / WB
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() 
+{
+	
+	return 0;
+}
