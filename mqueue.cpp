fc77f4d8facac85d1c2c7b94d7ca0049644180d4
Bajad3rka <102176673+Bajad3rka@users.noreply.github.com>
2022-03-29 19:14:49 +0200

    1.2 constructor

--- mqueue.cpp
+++ mqueue.cpp
@@ -2,10 +2,11 @@
 class implementing queue in C++
 the same functionality as STL (Standard Template Library) class queue in C++
 with usage example
-version 1.1
+version 1.2
 Consists
 a. program template
 b. definition of class mqueue
+c. constructor
 Author: Bajadera / WB
 */
 #include <iostream>
@@ -14,12 +15,20 @@ using namespace std;
 
 class mqueue
 {
+	public:
+	mqueue() //Constructor
+	{
+		cout << "Jestem 1\n";
+		first = last = -1;
+		cout<< "Jestem 2\n";
+	}
+	private:
 	vector<int> elements;
 	int first, last;
 };
 int main() 
 {
 	mqueue payments;
-	
+	mqueue payments2;
 	return 0;
 }
